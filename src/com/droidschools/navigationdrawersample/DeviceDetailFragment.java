fd722ed8e90558e69a4072fffb2062fed6557895
ajithmemana <ajithmemana@outlook.com>
2013-05-24 14:51:55 +0530

    Fixed. Working code

---
+++ src/com/droidschools/navigationdrawersample/DeviceDetailFragment.java
@@ -0,0 +1,40 @@
+package com.droidschools.navigationdrawersample;
+
+import java.util.Locale;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class DeviceDetailFragment extends Fragment{
+
+
+    public static final String ARG_PHONE_INDEX = "phone_index";
+    String phoneImageName ;
+
+    public DeviceDetailFragment() {
+        // Empty constructor required for fragment subclasses
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.device_detail_fragment_layout, container, false);
+        int i = getArguments().getInt(ARG_PHONE_INDEX);
+        //Get the corresponding name from string array
+         phoneImageName = getResources().getStringArray(R.array.phone_array)[i];
+        
+// Get the resource Id usign name  -  filename.drawable.packagename
+        int imageId = getResources().getIdentifier(phoneImageName.toLowerCase(Locale.getDefault()),
+                        "drawable", getActivity().getPackageName());
+        ((ImageView) rootView.findViewById(R.id.deviceimageView)).setImageResource(imageId);
+        ((TextView) rootView.findViewById(R.id.phoneNametextView)).setText(phoneImageName);
+        getActivity().setTitle(imageId);
+        return rootView;
+    }
+
+}
--- src/com/droidschools/navigationdrawersample/MainActivity.java
+++ src/com/droidschools/navigationdrawersample/MainActivity.java
@@ -1,15 +1,39 @@
 package com.droidschools.navigationdrawersample;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnItemClickListener {
+	ListView drawerListView;
+    public static final String ARG_PHONE_INDEX = "phone_index";
+
+
+	String[] listItems = {
+			"Nokia Lumia 920",
+			"iPhone 5s",
+			"Nokia Lumia 925",
+			"Xperia Z",
+			"HTC One"
+	};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		drawerListView = (ListView) findViewById(R.id.left_drawer);
+		drawerListView.setAdapter(new ArrayAdapter<String>(getBaseContext(), R.layout.listview_layout_cell,listItems));
+		drawerListView.setDividerHeight(2);
+		drawerListView.setOnItemClickListener(this);
+		
 	}
 
 	@Override
@@ -17,6 +41,19 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> arg0, View arg1, int position, long arg3) {
+		Toast.makeText(getBaseContext(), "Clicked " + position, 0).show();
+		Fragment deviceDetailFragment = new DeviceDetailFragment();
+		Bundle argumentsPassed = new Bundle();
+		argumentsPassed.putInt(ARG_PHONE_INDEX, position);
+		deviceDetailFragment.setArguments(argumentsPassed);
+		FragmentManager  fragmentManager = getFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.content_frame, deviceDetailFragment).commit();
+
+
 	}
 
 }
